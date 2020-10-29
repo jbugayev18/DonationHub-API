86a88e67fdf70ffabe78543fbf0c63ca07a165d7
John Adney <jmoon5ftm@gmail.com>
2020-10-29 12:51:41 -0700

    bugfix: use the index

--- src/site/site-service.js
+++ src/site/site-service.js
@@ -1,14 +1,14 @@
 const boxQuery =
-  "box(point(lon, lat), point(lon, lat)) && '(??, ??), (??, ??)'::box";
+  "box(point(lat, lon), point(lat, lon)) && '(??, ??), (??, ??)'::box";
 
 const SitesService = {
   // TODO -- do something about the seam at +/-180 degrees longitude
   getSitesInWindow(db, leftLon, topLat, rightLon, bottomLat) {
     return db("site").whereRaw(boxQuery, [
-      +leftLon || 0,
       +topLat || 0,
-      +rightLon || 0,
+      +leftLon || 0,
       +bottomLat || 0,
+      +rightLon || 0,
     ]);
   },
   postSite(db, newSite) {
