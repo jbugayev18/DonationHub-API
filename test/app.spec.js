15d279ed53b87896a052b8bf0021c25f05887d8b
Kayleigh Young <64609499+kayleighkat98@users.noreply.github.com>
2020-10-29 17:41:40 -0500

    add supertest to app spec

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,5 +1,5 @@
 const app = require('../src/app');
-
+const {supertest} = require('./setup');
 describe('App', () => {
   it('GET / responds with 200 containing "Hello, world!"', () => {
     return supertest(app)
