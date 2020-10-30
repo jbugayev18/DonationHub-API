e58f93a4e979c03674f495f960ba39ecea60ae92
Kayleigh Young <64609499+kayleighkat98@users.noreply.github.com>
2020-10-29 22:00:20 -0500

    post site is working!

--- src/site/site-router.js
+++ src/site/site-router.js
@@ -5,35 +5,35 @@ const SitesService = require("./site-service");
 const {sanitizeFields} = require('../utils');
 const jsonParser = express.json();
 
+
 siteRouter.get("/", async (req, res, next) => {
-  try {
-    return res.json(await SitesService.getSitesInWindow(req.app.get("db"), ...String(req.query.rect).split(',',4)));
-  } catch(err) {
-    next(err);
-  }
-});
+    try {
+      return res.json(await SitesService.getSitesInWindow(req.app.get("db"), ...String(req.query.rect).split(',',4)));
+    } catch(err) {
+      next(err);
+    }
+  });
 
 siteRouter.use(requireAuth).route("/")
-.post(jsonParser,async (req, res, next) => {
-  const db = req.app.get('db');
-  const {lat, lon,label, address, description, place_id} = req.body;
-  let newSite = {  lat, lon,label, address, description, place_id };
-  
-  for (const [key, value] of Object.entries(newSite)){
-    if(value === null){
-      return next({status: 400, message: `Missing '${key}' in request body`});
+  .post(jsonParser,async (req, res, next) => {
+    const db = req.app.get('db');
+    const {lat, lon,label, address, description, place_id} = req.body;
+    let newSite = {  lat, lon,label, address, description, place_id };
+    for (const [key, value] of Object.entries(newSite)){
+      if(value === null){
+        return next({status: 400, message: `Missing '${key}' in request body`});
+      }
     }
-  }
-   newSite = sanitizeFields(newSite);
-  try {
-    const sites = await SitesService.postSite(db, newSite);
-    res
-      .status(201)
-      .json(sites);
-  } catch (err) {
-    next(err);
-  }
-  //return res.status(500).send();
-});
+    newSite = sanitizeFields(newSite);
+    try {
+      const sites = await SitesService.postSite(db, newSite);
+      res
+        .status(201)
+        .json(sites);
+    } catch (err) {
+      next(err);
+    }
+    //return res.status(500).send();
+  });
 
 module.exports = siteRouter;
