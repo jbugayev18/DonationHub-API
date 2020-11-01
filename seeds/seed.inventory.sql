c6d3db4cc0a903c9f3cdc5291feeadaa4d0f6eff
Kayleigh Young <64609499+kayleighkat98@users.noreply.github.com>
2020-11-01 03:40:17 -0600

    add migrate to scripts and fix inventory seed issue

--- seeds/seed.inventory.sql
+++ seeds/seed.inventory.sql
@@ -8,3 +8,6 @@ INSERT INTO inventory (id, item_name, site_id, ideal_amount, current_amount, cri
     ('3', 'Body Wash', '2', '100', '20', '60'),
     ('4', 'Toothbrush', '2', '120', '50', '80'),
     ('5', 'Mouth wash', '3', '50', '10', '30')
+;
+
+commit;
